21844ed4eb4f76777c8b92094f6d7a26a2b2f5da
Micael Teixeira <mikatx2@hotmail.com>
2022-03-29 20:34:38 +0100

    added some board creation code

--- SnakesAndLadders/Program.cs
+++ SnakesAndLadders/Program.cs
@@ -6,7 +6,18 @@ namespace SnakesAndLadders
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Draw the map
+            int boardX = 5;
+            int boardY = 5;
+
+            int[][] board = new int[boardX][boardY];
+
+            board[0][1] = 1;
+
+            Console.WriteLine(board[0][1]);
+
+            
         }
     }
 }
