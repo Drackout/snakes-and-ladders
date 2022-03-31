107eda4afe6467fe94dd28c6dacc8d34f830fe40
Micael Teixeira <mikatx2@hotmail.com>
2022-04-01 00:23:08 +0100

    Create Board, Add Values, Draw Board on Console

--- SnakesAndLadders/Program.cs
+++ SnakesAndLadders/Program.cs
@@ -7,17 +7,74 @@ namespace SnakesAndLadders
         static void Main(string[] args)
         {
 
-            //Draw the map
+            //Board Size Lines and Columns
             int boardX = 5;
             int boardY = 5;
 
-            int[][] board = new int[boardX][boardY];
+            //int linhaDeJogo = boardX * boardY;
 
-            board[0][1] = 1;
+            //Create the board with the Lines and Columns
+            int[,] board = new int[boardX, boardY];
 
-            Console.WriteLine(board[0][1]);
+            board = FillBoard(board);
 
-            
+            //DrawBoard(board);
+
+
+            //Draw Board | TODO Invert the array and write le zig zag
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    Console.Write($"{board[i,j]} | ");
+                }
+                Console.WriteLine();
+            }
+
+            /*
+            board[0,1] = 1;
+            Console.WriteLine(board[0,1]);
+            */
+
+
         }
+
+        /// <summary>
+        /// Get created board, fill it with values
+        /// </summary>
+        /// <param name="board">Board of the game</param>
+        private static int[,] FillBoard(int[,] board)
+        {
+            int tileNumber = 0;
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i, j] = tileNumber;
+                    tileNumber++;
+                }
+            }
+            return board;
+        }
+
+        /*
+        /// <summary>
+        /// Draws board to the console
+        /// </summary>
+        /// <param name="board">Board of the game</param>
+        private static void DrawBoard(int[,] board){
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    Console.Write($"{board[i,j]}");
+                }
+                Console.WriteLine();
+            }
+            return 0;
+        }
+        */
+
     }
 }
